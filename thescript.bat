9dea3e95df7ddace67ef357bb2f11c490171dc5e
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2015-08-24 15:21:01 +0100

    pgtune-script for windows
    Contains PGTUNE from Greg Smith and Python 2.7.10

---
+++ thescript.bat
@@ -0,0 +1,15 @@
+
+wmic logicaldisk get caption,description,filesystem
+@echo off
+set /p drive="Enter the letter assigned to the USB drive: "
+set PYTHON=C:\Python27
+set PYTHONPATH=%PYTHONPATH%;C:\Python27\Lib;C:\Python27\DLLs;C:\Python27\Lib\lib-tk;C:\Python27;%drive%:\PGTUNE
+set /p postgresqlversion="Enter the PostgreSQL version (ex 9.1): "
+set postgrespath="C:\Program Files\PostgreSQL\%postgresqlversion%\data\postgresql.conf"
+set newpath="C:\Program Files\PostgreSQL\%postgresqlversion%\data\postgresql.conf.pgtune"
+@echo on
+cd C:\Python27
+python %drive%:\PGTUNE\pgtune -i %postgrespath% -o %postgrespath%
+@echo off
+echo All is done...
+PAUSE
