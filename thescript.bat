bebb01b0e69d83caa0d5ed61ad3167e19aea7a33
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2015-08-25 11:49:28 +0100

    The script now backups our initial config file

--- thescript.bat
+++ thescript.bat
@@ -6,10 +6,15 @@ set PYTHON=C:\Python27
 set PYTHONPATH=%PYTHONPATH%;C:\Python27\Lib;C:\Python27\DLLs;C:\Python27\Lib\lib-tk;C:\Python27;%drive%:\PGTUNE
 set /p postgresqlversion="Enter the PostgreSQL version (ex 9.1): "
 set postgrespath="C:\Program Files\PostgreSQL\%postgresqlversion%\data\postgresql.conf"
-set newpath="C:\Program Files\PostgreSQL\%postgresqlversion%\data\postgresql.conf.pgtune"
-@echo on
+set newpath="C:\Program Files\PostgreSQL\%postgresqlversion%\data\postgresql.conf.backup"
+echo.
+xcopy %postgrespath% %newpath% /H /Y
+echo.
+echo.
 cd /d C:\Python27
 python %drive%:\PGTUNE\pgtune -i %postgrespath% -o %postgrespath%
-@echo off
+echo.
 echo All is done...
+echo.
+echo.
 PAUSE
